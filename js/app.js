fa62dde38d40f4a23a87e315c0ccc985810a1d50
Jayman Matthews <jaymanmatthews32@gmail.com>
2021-03-05 23:29:35 +0800

    P.O.C -2.33
    Implemented engineering and infinity notations for number formatting. Also implemented a basic hand mechanic that increases the number of apples gained per second. NOTE: This mechanic is still extremely experimental and unfinished. I will probably rewrite the logic for hand to apples/ps calculations etc.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ const nD = val => new Decimal(val)
 
 function DefaultPlayer() {
   return {
-    coins: {
-      current: nD(0)
+    apples: {
+      current: nD(0),
+      per: {
+        sec: nD(2)
+      }
+    },
+    hands: {
+      current: nD(2),
+      cost: nD(50)
     },
     notations: {
       current: "Standard"
@@ -21,15 +28,38 @@ const store = new Vuex.Store({
   getters: {
     currentValues: state => {
       return {
-        coins: state.coins.current,
+        apples: state.apples.current,
+        hands: state.hands.current,
         notation: state.notations.current,
         tab: state.tabs.current
+      }
+    },
+    perSecValues: state => {
+      return {
+        apples: state.apples.per.sec
+      }
+    },
+    costValues: state => {
+      return {
+        hands: state.hands.cost
       }
     }
   },
   mutations: {
-    INCREMENT_COINS: (state, payload) => {
-      state.coins.current = state.coins.current.add(payload);
+    INCREMENT_APPLES: (state, payload) => {
+      state.apples.current = state.apples.current.add(payload);
+    },
+    DECREMENT_APPLES: (state, payload) => {
+      state.apples.current = state.apples.current.sub(payload);
+    },
+    INCREMENT_APPLES_PS: (state, payload) => {
+      state.apples.per.sec = state.apples.per.sec.add(payload);
+    },
+    INCREMENT_HANDS: (state, payload) => {
+      state.hands.current = state.hands.current.add(payload);
+    },
+    SCALE_HAND_COST: (state, payload) => {
+      state.hands.cost = state.hands.cost.mul(payload);
     },
     SWITCH_TAB: (state, payload) => {
       state.tabs.current = payload;
@@ -44,29 +74,38 @@ const app = new Vue({
     this.req = requestAnimationFrame(this.tick);
     this.time = 0;
     this.saveInterval = setInterval(this.save, 15000);
-    this.notes = ["Standard", "Scientific", "Logarithmic", "Mixed Scientific", "Alphabetic"];
+    this.notes = ["Standard", "Scientific", "Logarithmic", "Mixed Scientific", "Alphabetic", "Engineering", "Infinity"];
     this.letters = ['K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp'];
     this.alphabet = `abcdefghijklmnopqrstuvwxyz`;
   },
   data: () => store.state,
   computed: {
     ...Vuex.mapGetters({
-      current: "currentValues"
+      current: "currentValues",
+      per_sec: "perSecValues",
+      cost: "costValues"
     }),
     formatted() {
       return {
-        coins: this.format(this.current.notation, this.current.coins)
+        apples: this.format(this.current.notation, this.current.apples),
+        apples_per_sec: this.format(this.current.notation, this.per_sec.apples),
+        hands: this.format(this.current.notation, this.current.hands),
+        hand_cost: this.format(this.current.notation, this.cost.hands)
       }
     }
   },
   methods: {
     ...Vuex.mapMutations({
-      incrementCoins: "INCREMENT_COINS",
+      incrementApples: "INCREMENT_APPLES",
+      decrementApples: "DECREMENT_APPLES",
+      incrementApplesPS: "INCREMENT_APPLES_PS",
+      incrementHands: "INCREMENT_HANDS",
+      scaleHandCost: "SCALE_HAND_COST",
       switchTab: "SWITCH_TAB"
     }),
     tick(timeTaken) {
       const deltaTime = timeTaken - this.time;
-      this.incrementCoins(nD(1e307).mul(deltaTime / 1000));
+      this.incrementApples(nD(this.per_sec.apples).mul(deltaTime / 1000));
       this.time = timeTaken;
       this.req = requestAnimationFrame(this.tick);
     },
@@ -77,6 +116,14 @@ const app = new Vue({
     reset() {
       localStorage.removeItem("idle_save");
       location.reload();
+    },
+    buyHand() {
+      if (this.current.apples.gte(this.cost.hands)) {
+        this.incrementHands(1);
+        this.incrementApplesPS(1);
+        this.decrementApples(this.cost.hands);
+        this.scaleHandCost(1.35);
+      }
     },
     changeNotation() {
       let isNotLastNote = this.notes.indexOf(this.notations.current) != this.notes.length - 1;
@@ -97,7 +144,9 @@ const app = new Vue({
       let y = nD(index / 26).floor();
       let character1 = this.alphabet.charAt(y);
       let character2 = this.alphabet.charAt(index % 26);
-
+      let engineered = nD(exponent / 3).floor().times(3);
+      let infinity = nD(Number.MAX_VALUE).log10();
+      
       switch (note.toLowerCase()) {
         case "standard":
           mantissa = val.div(nD(10).pow(power));
@@ -119,9 +168,18 @@ const app = new Vue({
           mantissa = val.div(nD(10).pow(power));
           string = `${mantissa.toFixed(2)}${character1}${character2}`;
           break;
+        case "engineering":
+          // 1e4 I want it to be 10e3, 1e5 = 100e3, 1e6 = 1e6
+          mantissa = val.div(nD(10).pow(power));
+          string = `${mantissa.toFixed(2)}e${engineered}`;
+          break;
+        case "infinity":
+          // calculate infinities based on powers of 10 (using log 10)
+          mantissa = val.log10().div(infinity);
+          string = `${mantissa.toFixed(2)} ∞`;
       }
     
-      return exponent >= 3 ? string : val.toFixed(2);
+      return exponent >= 3 ? string : val.toFixed(0);
     
     
     
